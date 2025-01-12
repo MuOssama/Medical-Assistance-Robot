129221cdc0396232db7c363613bb605bd389afcb
Mustapha Ossama <109214165+MuOssama@users.noreply.github.com>
2025-01-12 22:22:10 +0200

    ros addition

--- Despenser/Despenser.ino
+++ Despenser/Despenser.ino
@@ -1,9 +1,11 @@
-//despenser node
 #include <ros.h>
 #include <Servo.h>
-#include <std_msgs/Int32.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Int32.h>
 #include <std_msgs/Int32MultiArray.h>
+
+// ROS node handle
+ros::NodeHandle nh;
 
 // Pin Definitions
 const int WATER_PUMP_PIN = 2;
@@ -18,10 +20,17 @@ const int IR_WATER_PIN = 7;
 const int IR_SYRUP_PIN = 8;
 const int IR_TABLET_PIN = 9;
 
+// Status LEDs
+const int STATUS_LED_DISPENSING = 10;
+const int STATUS_LED_READY = 11;
+const int STATUS_LED_ERROR = 12;
+
 // Timing Constants
-const int PUMP_DURATION = 3000;  // 3 seconds for pumps
-const int SERVO_DURATION = 1000; // 1 second for servo movement
-const int DANGER_LED_DURATION = 1000; // 1 second for danger LED
+const int PUMP_DURATION = 3000;    // 3 seconds for pumps
+const int SERVO_DURATION = 1000;   // 1 second for servo movement
+const int DANGER_LED_DURATION = 1000;
+const int ERROR_TIMEOUT = 10000;   // 10 second timeout for dispensing
+const int SENSOR_CHECK_DELAY = 100; // 100ms between sensor checks
 
 // Servo positions
 const int SERVO_START = 0;
@@ -31,127 +40,280 @@ const int SERVO_END = 180;
 Servo tablet1Servo;
 Servo tablet2Servo;
 bool isDispensing = false;
+bool isPatientPresent = false;
 unsigned long dispensingStartTime = 0;
+unsigned long lastSensorCheckTime = 0;
 int currentDispenseStep = 0;
 int medications[5] = {0, 0, 0, 0, 0}; // [water, syrup1, syrup2, tablet1, tablet2]
+int currentPatientId = 0;
+bool systemHalted = false;
+int dispensingErrors = 0;
+const int MAX_DISPENSING_RETRIES = 3;
 
-// ROS setup
-ros::NodeHandle nh;
+// Publishers
+std_msgs::Bool halt_msg;
+ros::Publisher halt_pub("halt_system", &halt_msg);
 
+// Status message publisher
+std_msgs::Int32 status_msg;
+ros::Publisher status_pub("dispenser_status", &status_msg);
+
+// Function declarations
+void initializeDispenser();
+bool checkSensor(int sensorPin);
+void setStatusLEDs(bool dispensing, bool ready, bool error);
+bool dispenseWithRetry(void (*dispenseFunc)(), int sensorPin);
+void handleDispenseError();
+void resetDispenser();
+
+// Callback for patient reached message
+void reachedCallback(const std_msgs::Bool& msg) {
+    isPatientPresent = msg.data;
+    if(isPatientPresent && !isDispensing) {
+        // Start dispensing process
+        isDispensing = true;
+        currentDispenseStep = 0;
+        dispensingStartTime = millis();
+        dispensingErrors = 0;
+        
+        // Halt the robot
+        halt_msg.data = true;
+        halt_pub.publish(&halt_msg);
+        
+        // Set status LEDs
+        setStatusLEDs(true, false, false);
+        
+        // Publish status
+        status_msg.data = 1; // 1 = Started dispensing
+        status_pub.publish(&status_msg);
+    }
+}
+
+// Callback for current patient ID
+void patientCallback(const std_msgs::Int32& msg) {
+    currentPatientId = msg.data;
+}
+
+// Callback for medication array
 void medicationCallback(const std_msgs::Int32MultiArray& msg) {
-  if (isDispensing) return; // Don't start new dispense if currently dispensing
-  
-  // Copy medication array
-  for (int i = 0; i < 5; i++) {
-    medications[i] = msg.data[i];
-  }
-  
-  // Start dispensing process
-  isDispensing = true;
-  currentDispenseStep = 0;
-  dispensingStartTime = millis();
+    if(msg.data.size() == 5) {
+        for(int i = 0; i < 5; i++) {
+            medications[i] = msg.data[i];
+        }
+    }
 }
 
-void dangerCallback(const std_msgs::Bool& msg) {
-  if (msg.data) {
-    digitalWrite(DANGER_LED_PIN, HIGH);
-    delay(DANGER_LED_DURATION);
-    digitalWrite(DANGER_LED_PIN, LOW);
-  }
-}
-
-ros::Subscriber<std_msgs::Int32MultiArray> medSub("medication_array", medicationCallback);
-ros::Subscriber<std_msgs::Bool> dangerSub("patient_danger", dangerCallback);
+// ROS subscribers
+ros::Subscriber<std_msgs::Bool> reached_sub("patient_reached", &reachedCallback);
+ros::Subscriber<std_msgs::Int32> patient_sub("current_patient", &patientCallback);
+ros::Subscriber<std_msgs::Int32MultiArray> med_sub("medication_array", &medicationCallback);
 
 void setup() {
-  // Initialize pins
-  pinMode(WATER_PUMP_PIN, OUTPUT);
-  pinMode(SYRUP1_PUMP_PIN, OUTPUT);
-  pinMode(SYRUP2_PUMP_PIN, OUTPUT);
-  pinMode(DANGER_LED_PIN, OUTPUT);
-  pinMode(IR_WATER_PIN, INPUT);
-  pinMode(IR_SYRUP_PIN, INPUT);
-  pinMode(IR_TABLET_PIN, INPUT);
-  
-  // Initialize servos
-  tablet1Servo.attach(TABLET1_SERVO_PIN);
-  tablet2Servo.attach(TABLET2_SERVO_PIN);
-  tablet1Servo.write(SERVO_START);
-  tablet2Servo.write(SERVO_START);
-  
-  // Initialize ROS
-  nh.initNode();
-  nh.subscribe(medSub);
-  nh.subscribe(dangerSub);
+    // Initialize ROS
+    nh.initNode();
+    nh.subscribe(reached_sub);
+    nh.subscribe(patient_sub);
+    nh.subscribe(med_sub);
+    nh.advertise(halt_pub);
+    nh.advertise(status_pub);
+    
+    initializeDispenser();
 }
 
-bool checkIRSensor(int sensorPin) {
-  return digitalRead(sensorPin) == LOW; // LOW means object detected
+void initializeDispenser() {
+    // Initialize pins
+    pinMode(WATER_PUMP_PIN, OUTPUT);
+    pinMode(SYRUP1_PUMP_PIN, OUTPUT);
+    pinMode(SYRUP2_PUMP_PIN, OUTPUT);
+    pinMode(DANGER_LED_PIN, OUTPUT);
+    pinMode(STATUS_LED_DISPENSING, OUTPUT);
+    pinMode(STATUS_LED_READY, OUTPUT);
+    pinMode(STATUS_LED_ERROR, OUTPUT);
+    pinMode(IR_WATER_PIN, INPUT);
+    pinMode(IR_SYRUP_PIN, INPUT);
+    pinMode(IR_TABLET_PIN, INPUT);
+    
+    // Initialize servos
+    tablet1Servo.attach(TABLET1_SERVO_PIN);
+    tablet2Servo.attach(TABLET2_SERVO_PIN);
+    tablet1Servo.write(SERVO_START);
+    tablet2Servo.write(SERVO_START);
+    
+    // Set initial status
+    setStatusLEDs(false, true, false);
+}
+
+void setStatusLEDs(bool dispensing, bool ready, bool error) {
+    digitalWrite(STATUS_LED_DISPENSING, dispensing);
+    digitalWrite(STATUS_LED_READY, ready);
+    digitalWrite(STATUS_LED_ERROR, error);
+}
+
+bool checkSensor(int sensorPin) {
+    int readings = 0;
+    for(int i = 0; i < 5; i++) {  // Take 5 readings
+        readings += digitalRead(sensorPin);
+        delay(10);
+    }
+    return readings >= 3;  // Return true if majority of readings detect object
 }
 
 void dispenseWater() {
-  if (!checkIRSensor(IR_WATER_PIN)) return;
-  digitalWrite(WATER_PUMP_PIN, HIGH);
-  delay(PUMP_DURATION);
-  digitalWrite(WATER_PUMP_PIN, LOW);
+    digitalWrite(WATER_PUMP_PIN, HIGH);
+    delay(PUMP_DURATION);
+    digitalWrite(WATER_PUMP_PIN, LOW);
 }
 
 void dispenseSyrup(int pumpPin) {
-  if (!checkIRSensor(IR_SYRUP_PIN)) return;
-  digitalWrite(pumpPin, HIGH);
-  delay(PUMP_DURATION);
-  digitalWrite(pumpPin, LOW);
+    digitalWrite(pumpPin, HIGH);
+    delay(PUMP_DURATION);
+    digitalWrite(pumpPin, LOW);
 }
 
 void dispenseTablet(Servo& servo) {
-  if (!checkIRSensor(IR_TABLET_PIN)) return;
-  servo.write(SERVO_END);
-  delay(SERVO_DURATION);
-  servo.write(SERVO_START);
+    servo.write(SERVO_END);
+    delay(SERVO_DURATION);
+    servo.write(SERVO_START);
+    delay(SERVO_DURATION);  // Wait for tablet to fall
+}
+
+bool dispenseWithRetry(void (*dispenseFunc)(), int sensorPin) {
+    for(int retry = 0; retry < MAX_DISPENSING_RETRIES; retry++) {
+        if(!checkSensor(sensorPin)) {
+            dispensingErrors++;
+            delay(1000);  // Wait before retry
+            continue;
+        }
+        
+        dispenseFunc();
+        delay(500);  // Wait for dispensing to complete
+        
+        if(checkSensor(sensorPin)) {
+            return true;
+        }
+        
+        dispensingErrors++;
+        delay(1000);  // Wait before retry
+    }
+    return false;
+}
+
+void handleDispenseError() {
+    setStatusLEDs(false, false, true);
+    status_msg.data = -1;  // -1 = Error
+    status_pub.publish(&status_msg);
+    
+    // Reset the dispenser
+    resetDispenser();
+}
+
+void resetDispenser() {
+    isDispensing = false;
+    currentDispenseStep = 0;
+    dispensingErrors = 0;
+    
+    // Turn off all pumps
+    digitalWrite(WATER_PUMP_PIN, LOW);
+    digitalWrite(SYRUP1_PUMP_PIN, LOW);
+    digitalWrite(SYRUP2_PUMP_PIN, LOW);
+    
+    // Reset servos
+    tablet1Servo.write(SERVO_START);
+    tablet2Servo.write(SERVO_START);
+    
+    // Release halt
+    halt_msg.data = false;
+    halt_pub.publish(&halt_msg);
+    
+    // Reset status
+    setStatusLEDs(false, true, false);
 }
 
 void handleDispensing() {
-  if (!isDispensing) return;
-  
-  unsigned long currentTime = millis();
-  
-  // State machine for dispensing
-  switch (currentDispenseStep) {
-    case 0: // Water
-      if (medications[0]) dispenseWater();
-      currentDispenseStep++;
-      break;
-      
-    case 1: // Syrup 1
-      if (medications[1]) dispenseSyrup(SYRUP1_PUMP_PIN);
-      currentDispenseStep++;
-      break;
-      
-    case 2: // Syrup 2
-      if (medications[2]) dispenseSyrup(SYRUP2_PUMP_PIN);
-      currentDispenseStep++;
-      break;
-      
-    case 3: // Tablet 1
-      if (medications[3]) dispenseTablet(tablet1Servo);
-      currentDispenseStep++;
-      break;
-      
-    case 4: // Tablet 2
-      if (medications[4]) dispenseTablet(tablet2Servo);
-      currentDispenseStep++;
-      break;
-      
-    default:
-      // Reset dispensing state
-      isDispensing = false;
-      currentDispenseStep = 0;
-      break;
-  }
+    if(!isDispensing) return;
+    
+    // Check for timeout
+    if(millis() - dispensingStartTime > ERROR_TIMEOUT) {
+        handleDispenseError();
+        return;
+    }
+    
+    // Check for too many errors
+    if(dispensingErrors > MAX_DISPENSING_RETRIES * 2) {
+        handleDispenseError();
+        return;
+    }
+    
+    switch(currentDispenseStep) {
+        case 0:  // Water
+            if(medications[0]) {
+                if(dispenseWithRetry(dispenseWater, IR_WATER_PIN)) {
+                    currentDispenseStep++;
+                }
+            } else {
+                currentDispenseStep++;
+            }
+            break;
+            
+        case 1:  // Syrup 1
+            if(medications[1]) {
+                if(dispenseWithRetry([]() { dispenseSyrup(SYRUP1_PUMP_PIN); }, IR_SYRUP_PIN)) {
+                    currentDispenseStep++;
+                }
+            } else {
+                currentDispenseStep++;
+            }
+            break;
+            
+        case 2:  // Syrup 2
+            if(medications[2]) {
+                if(dispenseWithRetry([]() { dispenseSyrup(SYRUP2_PUMP_PIN); }, IR_SYRUP_PIN)) {
+                    currentDispenseStep++;
+                }
+            } else {
+                currentDispenseStep++;
+            }
+            break;
+            
+        case 3:  // Tablet 1
+            if(medications[3]) {
+                if(dispenseWithRetry([]() { dispenseTablet(tablet1Servo); }, IR_TABLET_PIN)) {
+                    currentDispenseStep++;
+                }
+            } else {
+                currentDispenseStep++;
+            }
+            break;
+            
+        case 4:  // Tablet 2
+            if(medications[4]) {
+                if(dispenseWithRetry([]() { dispenseTablet(tablet2Servo); }, IR_TABLET_PIN)) {
+                    currentDispenseStep++;
+                }
+            } else {
+                currentDispenseStep++;
+            }
+            break;
+            
+        default:
+            // Dispensing complete
+            status_msg.data = 2;  // 2 = Completed successfully
+            status_pub.publish(&status_msg);
+            
+            // Reset and release halt
+            resetDispenser();
+            break;
+    }
 }
 
 void loop() {
-  nh.spinOnce();
-  handleDispensing();
-  delay(100);
+    nh.spinOnce();
+    
+    // Only check sensors periodically to avoid overwhelming the system
+    if(millis() - lastSensorCheckTime >= SENSOR_CHECK_DELAY) {
+        handleDispensing();
+        lastSensorCheckTime = millis();
+    }
+    
+    delay(10);
 }
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import sqlite3
 import json
 from datetime import datetime
 import rospy
-from std_msgs.msg import Int32MultiArray, Bool, MultiArrayDimension
+from std_msgs.msg import Int32MultiArray, Bool, MultiArrayDimension, Int32
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from threading import Thread
@@ -16,6 +16,16 @@ CORS(app)
 rospy.init_node('flask_server', anonymous=True)
 medication_pub = rospy.Publisher('medication_array', Int32MultiArray, queue_size=10)
 danger_pub = rospy.Publisher('patient_danger', Bool, queue_size=10)
+
+# Global variable for tracking current patient
+current_patient = 0
+
+def patient_callback(msg):
+    global current_patient
+    current_patient = msg.data
+    publish_patient_medications(current_patient)
+
+rospy.Subscriber('current_patient', Int32, patient_callback)
 
 def init_db():
     conn = sqlite3.connect('patients.db')
@@ -32,15 +42,15 @@ def init_db():
                   patient_order INTEGER)
               ''')
     
-    # Initialize with fixed orders
-    c.execute('''INSERT OR IGNORE INTO patients VALUES (1, 1, NULL, NULL, NULL, '[]', NULL, NULL, 1)''')
-    c.execute('''INSERT OR IGNORE INTO patients VALUES (2, 2, NULL, NULL, NULL, '[]', NULL, NULL, 2)''')
-    c.execute('''INSERT OR IGNORE INTO patients VALUES (3, 3, NULL, NULL, NULL, '[]', NULL, NULL, 3)''')
+    # Initialize with fixed orders and medication arrays
+    default_medicals = json.dumps([0, 0, 0, 0, 0])  # [water, syrup1, syrup2, tablet1, tablet2]
+    c.execute('''INSERT OR IGNORE INTO patients VALUES (1, 1, NULL, NULL, NULL, ?, NULL, NULL, 1)''', (default_medicals,))
+    c.execute('''INSERT OR IGNORE INTO patients VALUES (2, 2, NULL, NULL, NULL, ?, NULL, NULL, 2)''', (default_medicals,))
+    c.execute('''INSERT OR IGNORE INTO patients VALUES (3, 3, NULL, NULL, NULL, ?, NULL, NULL, 3)''', (default_medicals,))
     conn.commit()
     conn.close()
 
 def check_health_state(hr, spo2, temp, glucose):
-    """Evaluate health state based on vital signs."""
     if any(v is None for v in [hr, spo2, temp, glucose]):
         return "unknown"
     return "in danger" if (
@@ -50,28 +60,29 @@ def check_health_state(hr, spo2, temp, glucose):
         glucose < 70 or glucose > 140
     ) else "ok"
 
-def publish_medication_array(patient_id, medicals):
-    """Publish medication array as ROS message."""
+def publish_patient_medications(patient_id):
     try:
-        med_array = json.loads(medicals)
-        if not isinstance(med_array, list) or len(med_array) != 5:
-            rospy.logwarn("Invalid medicals format for patient %s", patient_id)
-            return
-
-        msg = Int32MultiArray()
-        msg.layout.dim = [MultiArrayDimension()]
-        msg.layout.dim[0].size = 5
-        msg.layout.dim[0].stride = 1
-        msg.layout.dim[0].label = "patient_%d" % patient_id
-        msg.data = [int(bool(x)) for x in med_array]
-        medication_pub.publish(msg)
-        rospy.loginfo("Published medications for patient %d: %s", patient_id, str(msg.data))
-
+        conn = sqlite3.connect('patients.db')
+        c = conn.cursor()
+        c.execute('SELECT medicals FROM patients WHERE id = ?', (patient_id,))
+        result = c.fetchone()
+        conn.close()
+        
+        if result:
+            medicals = json.loads(result[0])
+            msg = Int32MultiArray()
+            msg.layout.dim = [MultiArrayDimension()]
+            msg.layout.dim[0].size = 5
+            msg.layout.dim[0].stride = 1
+            msg.layout.dim[0].label = f"patient_{patient_id}"
+            msg.data = [int(bool(x)) for x in medicals]
+            medication_pub.publish(msg)
+            rospy.loginfo(f"Published medications for patient {patient_id}: {msg.data}")
+    
     except Exception as e:
-        rospy.logerr("Error publishing medication array: %s", str(e))
+        rospy.logerr(f"Error publishing medication array: {str(e)}")
 
 def publish_danger_state(is_in_danger):
-    """Publish danger state as ROS message."""
     try:
         msg = Bool()
         msg.data = is_in_danger
@@ -79,11 +90,10 @@ def publish_danger_state(is_in_danger):
         if is_in_danger:
             rospy.loginfo("Published danger state")
     except Exception as e:
-        rospy.logerr("Error publishing danger state: %s", str(e))
+        rospy.logerr(f"Error publishing danger state: {str(e)}")
 
 @app.route('/api/patients', methods=['GET'])
 def get_patients():
-    """Retrieve all patient records."""
     conn = sqlite3.connect('patients.db')
     c = conn.cursor()
     c.execute('SELECT * FROM patients ORDER BY patient_order')
@@ -134,8 +144,9 @@ def update_patient(patient_id):
         conn.commit()
         conn.close()
 
-        # Publish ROS messages after successful database update
-        publish_medication_array(patient_id, json.dumps(data['medicals']))
+        if patient_id == current_patient:
+            publish_patient_medications(patient_id)
+            
         health_state = check_health_state(
             float(data['heart_rate']),
             float(data['spo2']),
@@ -154,16 +165,11 @@ def index():
     return send_from_directory('static', 'index.html')
 
 def flask_thread():
-    """Function to run Flask in a separate thread."""
     app.run(debug=False, host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     init_db()
-    
-    # Start Flask in a separate thread
-    Thread(target=flask_thread).start()
-    
-    # Run ROS node
+    app.run(debug=False, host='0.0.0.0', port=5000)
     try:
         rospy.spin()
     except KeyboardInterrupt:
